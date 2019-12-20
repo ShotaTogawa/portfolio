66c2ab731e5b30027fc695ec2f34494079c334aa
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-19 23:09:55 -0800

    made skill page

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import classes from "./skills.css";
+import { frontEnd, backEnd, infrastructure, others } from "../../utils";
+
+class Skills extends Component {
+  state = {
+    field: frontEnd
+  };
+
+  renderSkills = () => {
+    return this.state.field.map(skill => {
+      return (
+        <div className="Skill" style={classes.Skill} key={skill.skill}>
+          <i className={skill.icon} style={{ color: skill.color }}></i>
+          <p className="Language" style={classes.Language}>
+            {skill.skill}
+          </p>
+          {skill.level === 1 ? (
+            <p>Speciality</p>
+          ) : skill.level === 2 ? (
+            <p>I know</p>
+          ) : (
+            <p>Studying now</p>
+          )}
+        </div>
+      );
+    });
+  };
+
+  handleClick = field => {
+    switch (field) {
+      case 0:
+        this.setState({ field: frontEnd });
+        break;
+      case 1:
+        this.setState({ field: backEnd });
+        break;
+      case 2:
+        this.setState({ field: infrastructure });
+        break;
+      case 3:
+        this.setState({ field: others });
+        break;
+      default:
+        break;
+    }
+  };
+  render() {
+    return (
+      <section id="skills">
+        <div className="Fields" style={classes.Fields}>
+          <div
+            className="Field"
+            style={classes.field}
+            onClick={() => this.handleClick(0)}
+          >
+            Frontend
+          </div>
+          <div
+            className="Field"
+            style={classes.field}
+            onClick={() => this.handleClick(1)}
+          >
+            Backend
+          </div>
+          <div
+            className="Field"
+            style={classes.field}
+            onClick={() => this.handleClick(2)}
+          >
+            Infrastructure
+          </div>
+          <div
+            className="Field"
+            style={classes.field}
+            onClick={() => this.handleClick(3)}
+          >
+            Others
+          </div>
+        </div>
+        {/* <h3 className="SkillsTitle" style={classes.SkillsTitle}>
+          My Skills
+        </h3> */}
+        <div className="Skills" style={classes.Skills}>
+          {this.renderSkills()}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Skills;
